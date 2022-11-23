9d01c1587756683f84bc32664bcb48965b96e419
yafet-123 <yafetaddisu123@gmail.com>
2022-11-23 14:34:56 -0800

    first commit

--- Database/Adduser.sql
+++ Database/Adduser.sql
@@ -6,3 +6,19 @@ VALUES
     ('nati','1234567'),
     ('dani','1234567')
 RETURNING *;
+
+
+INSERT INTO 
+    "Job" ("CompanyName","JobsType","Location","CareerLevel","EmploymentType","Salary","DeadLine","Apply","user_id")
+VALUES
+    ('hulu Media','Software developer','Addis abeba','0 year', 'Full time','10000', '2022-11-23 13:54:05.261739-08','go to bole', 2)
+RETURNING *;
+
+
+
+
+
+
+
+
+
