4902c2625a0898eb00e1f5b3b84b331df3f4ef8a
yafet-123 <yafetaddisu123@gmail.com>
2022-11-24 01:26:46 -0800

    adding controllers

--- routes/Category/CategoryRoute.js
+++ routes/Category/CategoryRoute.js
@@ -10,7 +10,7 @@ import {
 	deleteCategory
 } from '../../controllers/Category/CategoryController.js'
 
-router.route("/").post(admin,createCategory).get(admin,getAllCategory);
-router.route("/:id").delete(admin,deleteCategory).patch(admin,updateCategory).get(admin,getIndividualCategory);
+router.route("/").post(admin,createCategory).get(getAllCategory);
+router.route("/:id").delete(admin,deleteCategory).patch(admin,updateCategory).get(getIndividualCategory);
 
 export default router
