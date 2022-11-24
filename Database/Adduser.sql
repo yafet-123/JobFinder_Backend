02bbe83bc728f5da99dcea199ab8c9bea8e4ead3
yafet-123 <yafetaddisu123@gmail.com>
2022-11-24 01:17:03 -0800

    adding comment

--- Database/Adduser.sql
+++ Database/Adduser.sql
@@ -16,6 +16,33 @@ RETURNING *;
 
 
 
+INSERT INTO 
+    "ClinicList" ("ClinicName","Description","CreatedBy","IsActive")
+VALUES
+    ('Cardiac','Cardiac Description',1,true),
+    ('Central Triage','Central Triage',3,true),
+    ('Chest','Chest Description',1,true),
+    ('Dermatology','Dermatology Description',2,true),
+    ('Diabetic', 'Diabetic Description',5,true),
+    ('ENT','ENT Description',2,true),
+    ('General Surgery','General Surgery Description',2,true),
+    ('GI','GI Description',2,true),
+    ('Gynecology','Gynecology Description',2,true),
+    ('Hematology','Hematology Description',3,true),
+    ('Maxillofacial','Maxillofacial Description',1,true),
+    ('Minor OR','Minor OR Description',3,true),
+    ('Neurology','Neurology Description',5,true),
+    ('Neurosurgery','Neurosurgery Description',2,true),
+    ('Orthopaedics','Orthopaedics Description',3,true),
+    ('Physiotherapy','Physiotherapy Description',2,true),
+    ('Psychiatry','Psychiatry Description',1,true),
+    ('Renal','Renal Description',1,true),
+    ('Rheumatology','Rheumatology Description',1,true),
+    ('Staff Clinic','Staff Clinic Description',1,true)
+RETURNING *;
+
+
+
 
 
 
